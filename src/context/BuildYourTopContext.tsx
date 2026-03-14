d31da77846acb50fc75360f5392e81b38a67b774
botij0 <javier.gonzalez.soldado@gmail.com>
2026-03-14 10:42:52 +0100

    feat: update context

--- src/context/BuildYourTopContext.tsx
+++ src/context/BuildYourTopContext.tsx
@@ -1,11 +1,15 @@
 /* eslint-disable react-refresh/only-export-components */
 import type { Anime } from "@/interfaces/anime";
+import type { Manga } from "@/interfaces/manga";
 import { createContext, useEffect, useState } from "react";
 
 interface BuildYourTopContextProps {
   animeTopList: Record<number, Anime>;
+  mangaTopList: Record<number, Manga>;
   addAnimeToTopList: (position: number, anime: Anime) => void;
+  addMangaToTopList: (position: number, anime: Manga) => void;
   removeAnimeFromTopList: (position: number) => void;
+  removeMangaFromTopList: (position: number) => void;
 }
 
 export const BuildYourTopContext = createContext<BuildYourTopContextProps>({} as BuildYourTopContextProps);
@@ -15,11 +19,22 @@ export const getAnimeTopListFromLocalStorage = (): Record<number, Anime> => {
   return saved ? JSON.parse(saved) : {};
 }
 
+export const getMangaTopListFromLocalStorage = (): Record<number, Manga> => {
+  const saved = localStorage.getItem('mangaTopList');
+  return saved ? JSON.parse(saved) : {};
+}
+
 export const BuildYourTopProvider = ({ children }: { children: React.ReactNode }) => {
   const [animeTopList, setAnimeTopList] = useState<Record<number, Anime>>(getAnimeTopListFromLocalStorage());
+  const [mangaTopList, setMangaTopList] = useState<Record<number, Manga>>(getMangaTopListFromLocalStorage());
+
 
   const addAnimeToTopList = (position: number, anime: Anime) => {
     setAnimeTopList(prev => ({ ...prev, [position]: anime }));
+  }
+
+  const addMangaToTopList = (position: number, manga: Manga) => {
+    setMangaTopList(prev => ({ ...prev, [position]: manga }))
   }
 
   const removeAnimeFromTopList = (position: number) => {
@@ -30,16 +45,31 @@ export const BuildYourTopProvider = ({ children }: { children: React.ReactNode }
     });
   }
 
+  const removeMangaFromTopList = (position: number) => {
+    setMangaTopList(prev => {
+      const newTopList = { ...prev };
+      delete newTopList[position];
+      return newTopList;
+    })
+  }
+
   useEffect(() => {
     localStorage.setItem('animeTopList', JSON.stringify(animeTopList));
   }, [animeTopList]);
+
+  useEffect(() => {
+    localStorage.setItem('mangaTopList', JSON.stringify(mangaTopList));
+  }, [mangaTopList]);
 
   return (
     <BuildYourTopContext.Provider
       value={{
         animeTopList,
+        mangaTopList,
         addAnimeToTopList,
         removeAnimeFromTopList,
+        removeMangaFromTopList,
+        addMangaToTopList
       }}>
       {children}
     </BuildYourTopContext.Provider>
